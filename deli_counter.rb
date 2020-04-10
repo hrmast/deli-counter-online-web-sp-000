0886a3d6f4dfea9a700b9ca9d542f0921453f121
Heath Mast <hrmast@gmail.com>
2020-04-10 13:44:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,12 +13,8 @@ def now_serving(katz_deli)
 end
 
 def line(katz_deli)
-  katz_deli.each.with_index do |value, index| katz_deli.length + 1 > 0
+  if katz_deli.each.with_index do |value, index| katz_deli.length + 1 > 0
       puts "The line is currently: #{index + 1}. #{value}"
-  end
-
-  if katz_deli.length > 0
-    puts "The line is currently: #{katz_deli.join()}."
   else
     puts "The line is currently empty."
   end
