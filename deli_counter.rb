ad25dd37dbed7f0a700cea8f5ebb1cd35dda79e2
Heath Mast <hrmast@gmail.com>
2020-04-10 13:50:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ def line(katz_deli)
   end
 
   if katz_deli.length > 0
-    puts "The line is currently: #{list}."
+    puts "The line is currently: #{list.to_s}."
   else
     puts "The line is currently empty."
   end
